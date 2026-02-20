caeac254e5ad0f67c3f30859566e53a9fa9ac859
Rykerzeeke <amanmathssogani@gmail.com>
2026-02-21 01:40:00 +0530

    fixing broken pipeline due to server reboot

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.use("/api/telegram", telegramRoutes);
 
 const PORT = process.argv[2] || process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
